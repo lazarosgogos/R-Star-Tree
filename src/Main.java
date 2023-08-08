75dae499cd95162867cdd0886558be99733e9e3d
ChristodoulosZarogiannis <chriszaro@gmail.com>
2023-08-08 10:27:04 +0300

    implemented postOrder

--- src/Main.java
+++ src/Main.java
@@ -21,8 +21,13 @@ public class Main {
 
         Node root = BulkLoading.bulkLoading(entries, M);
 
-        //dfs(root,0);
-        BBS.runSkyline(root).forEach(pe -> System.out.println(pe.getPoint()));
+        //deapthFirstPrint(root,0);
+        //BBS.runSkyline(root).forEach(pe -> System.out.println(pe.getPoint()));
+
+        ArrayList<Node> postOrder = initPostOrder(root);
+        for (Node n : postOrder){
+            System.out.println(n);
+        }
 
         // KNN Query
 //        Point p = new Point(5, 5);
@@ -34,14 +39,14 @@ public class Main {
         Rectangle myQuery = new Rectangle(40.60987f, 22.96f, 40.61f, 22.967f);
         RangeQuery.rangeQuery(root, myQuery);*/
     }
-    public static void dfs(Node root, int level) {
+    public static void deapthFirstPrint(Node root, int level) {
         if (!root.leaf) {
             LinkedList<RectangleEntry> rectangles = ((NoLeafNode) root).getRectangleEntries();
             for (RectangleEntry rectangleEntry : rectangles) {
                 System.out.println("You are in Level: " + level);
                 System.out.println(rectangleEntry.getRectangle());
                 System.out.println("Going in...");
-                dfs(rectangleEntry.getChild(), level + 1);
+                deapthFirstPrint(rectangleEntry.getChild(), level + 1);
             }
         } else {
             System.out.println("Start Leaf Node");
@@ -52,25 +57,42 @@ public class Main {
         }
     }
 
-    public static ArrayList<Node> inorder(Node root){
+    public static ArrayList<Node> initPostOrder(Node root){
         ArrayList<Node> result = new ArrayList<>();
+
+        return postOrder(root, result);
+    }
+
+    public static ArrayList<Node> postOrder(Node root, ArrayList<Node> result){
 
         if (!root.leaf) {
             LinkedList<RectangleEntry> rectangles = ((NoLeafNode) root).getRectangleEntries();
             for (RectangleEntry rectangleEntry : rectangles) {
-                inorder(rectangleEntry.getChild());
-            }
-        } else {
-            for (PointEntry pointEntry : ((LeafNode) root).getPointEntries()) {
-                System.out.println(pointEntry.getPoint());
+                postOrder(rectangleEntry.getChild(), result);
             }
         }
+        result.add(root);
 
         return result;
     }
 
-    public static ArrayList<Node> preorder(Node root){
+    public static ArrayList<Node> initInOrder(Node root){
         ArrayList<Node> result = new ArrayList<>();
+
+        inOrder(root, result);
+
+        return result;
+    }
+
+    public static ArrayList<Node> inOrder(Node root, ArrayList<Node> result){
+
+        if (!root.leaf) {
+            LinkedList<RectangleEntry> rectangles = ((NoLeafNode) root).getRectangleEntries();
+            for (RectangleEntry rectangleEntry : rectangles) {
+                inOrder(rectangleEntry.getChild(), result);
+            }
+        }
+        result.add(root);
 
         return result;
     }
